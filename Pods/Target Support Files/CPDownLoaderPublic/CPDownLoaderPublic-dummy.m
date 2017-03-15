afc5fc0f1d8e70ee2ab3c0634ea8ad3d6fff14dd
ITEnthusiast <admin@caopei.cn>
2017-03-15 18:27:26 +0800

    第一天，回顾发布CocoaPods远程公开库大致流程

---
+++ Pods/Target Support Files/CPDownLoaderPublic/CPDownLoaderPublic-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CPDownLoaderPublic : NSObject
+@end
+@implementation PodsDummy_CPDownLoaderPublic
+@end
---
+++ Pods/Target Support Files/CPDownLoaderPublic/CPDownLoaderPublic-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "CPDownLoader.h"
+#import "CPFileTool.h"
+
+FOUNDATION_EXPORT double CPDownLoaderPublicVersionNumber;
+FOUNDATION_EXPORT const unsigned char CPDownLoaderPublicVersionString[];
+
